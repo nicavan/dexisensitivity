79a254c9e72d4cb24535eb8f16575a19ba8c7215
Roland Allart <roland.allart@inrae.fr>
2023-07-25 10:56:56 +0200

    doc(MonteCarlo) : add more documentation

--- R/MonteCarlo.R
+++ R/MonteCarlo.R
@@ -1,15 +1,17 @@
-#' Title
+#' Estimates the time required to perform a Monte Carlo simulation
 #'
-#' A simple function to give information on time requested to perform the MC
+#' Performs a time estimation for a specified number of Monte Carlo simulations.
+#' This estimation is based on the time taken to execute a smaller number of
+#' simulations specified by `iTest`.
 #'
-#' @param aTree aTree
-#' @param nbRuns nbRuns
-#' @param iTest iTest
+#' @param aTree Decision tree to run simulations on
+#' @param nbRuns Total number of Monte Carlo simulations to perform
+#' @param iTest Number of simulations to be used for time estimation
 #'
-#' @return
+#' @return No explicit return. The function prints out the estimated execution
+#'   time.
+#'
 #' @export
-#'
-#' @examples
 infoMC <- function(aTree,
                   nbRuns,
                   iTest = 50) {
@@ -35,19 +37,21 @@ infoMC <- function(aTree,
 
 
 
-#' Title
+#' Performs a Monte Carlo simulation
 #'
-#' MonteCarlo approach
+#' Conducts a Monte Carlo simulation on a given decision tree with a specified
+#' number of runs. If `isFile` is set to TRUE, a .csv file named "MC
+#' options.csv" is saved, which contains all the random options selected for the
+#' Monte Carlo analysis.
 #'
-#' @param aTree aTree
-#' @param nbRuns nbRuns
-#' @param isFile isFile
-#' @param verbose verbose
+#' @param aTree Decision tree to run the simulation on
+#' @param nbRuns Number of Monte Carlo simulations to perform
+#' @param isFile Logical value to decide whether to write a file or not
+#' @param verbose Logical value for printing additional information
 #'
-#' @return
+#' @return Returns a matrix representing the Monte Carlo simulations
+#'
 #' @export
-#'
-#' @examples
 MonteCarlo <- function(aTree,
                        nbRuns,
                        isFile = F,
@@ -89,18 +93,21 @@ MonteCarlo <- function(aTree,
 
 
 
-#' Title
+#' Displays the Monte Carlo simulation results
 #'
-#' @param Node Node
-#' @param MC MC
-#' @param nbRuns nbRuns
+#' This function generates a bar chart of the frequency of each outcome in the
+#' Monte Carlo simulation results for a given node. It also writes the bar
+#' lengths to a .csv file.
 #'
-#' @return
-#' @export
+#' @param Node Node of interest
+#' @param MC Monte Carlo simulation results
+#' @param nbRuns Number of Monte Carlo simulations performed
+#'
+#' @return Returns the bar chart data
 #'
 #' @importFrom graphics legend text
 #'
-#' @examples
+#' @export
 ShowMC <- function(Node,
                  MC,
                  nbRuns) {
