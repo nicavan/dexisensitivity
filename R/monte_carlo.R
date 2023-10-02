0e1a72683df40604b0b23464e24c854bfef24829
Roland Allart <roland.allart@inrae.fr>
2023-10-02 15:00:53 +0200

    refactor: improve clarity and structure of estimate_mc_time
    Replace sample generation method with create_options function. This change maintains similar computation performance while simplifying maintenance.

--- R/monte_carlo.R
+++ R/monte_carlo.R
@@ -1,40 +1,32 @@
-#' Estimates the time required to perform a Monte Carlo simulation
+#' Estimates execution time for Monte Carlo simulations
 #'
-#' Performs a time estimation for a specified number of Monte Carlo simulations.
-#' This estimation is based on the time taken to execute a smaller number of
-#' simulations specified by `iTest`.
+#' Estimates the time required to run a set number of Monte Carlo simulations
+#' based on the time taken to run a smaller test set.
 #'
-#' @param aTree Decision tree to run simulations on
-#' @param nbRuns Total number of Monte Carlo simulations to perform
-#' @param iTest Number of simulations to be used for time estimation
+#' @param tree Decision tree for simulations.
+#' @param num_runs Total number of simulations desired.
+#' @param num_test Number of test simulations for time estimation (default is
+#'   50).
 #'
-#' @return No explicit return. The function prints out the estimated execution
-#'   time.
-#'
+#' @return No explicit return. The function prints the estimated execution time.
 #' @export
-infoMC <- function(aTree,
-                  nbRuns,
-                  iTest = 50) {
-    start_time <- Sys.time()
-    option <- matrix(nrow = aTree@NumberOfLeaves,
-                     ncol = iTest)
-    rownames(option) <- aTree@Leaves
-    for(k in aTree@Leaves) {
-        option[k, ] <- aTree@Nodes[[get_id(aTree@Nodes,k)[1]]]@RangeScale %>%
-            sample(size = iTest,
-                   prob = aTree@Nodes[[get_id(aTree@Nodes, k)[1]]]@Probability,
-                   replace = TRUE)
-    }
+estimate_mc_time <- function(tree, num_runs, num_test = 50) {
 
-    dummy <- sapply(1:iTest,
-                    function(x) {evaluate_scenario(aTree,
-                                                  as.matrix(option[, x]))})
-    end_time <- Sys.time()
-    cat("\n Approximative required time to run MC with ",
-        nbRuns, " simulations",
-        (end_time-start_time)*nbRuns/iTest/60, " minutes")
+  # Generate sample options for test simulations using create_options function
+  options_matrix <- create_options(tree, num_options = num_test, seed = NULL)
+
+  # Run test simulations
+  start_time <- Sys.time()
+  sapply(1:num_test, function(x) {
+    evaluate_scenario(tree, as.matrix(options_matrix[, x]))
+  })
+  end_time <- Sys.time()
+
+  # Estimate and print the required time for desired simulations
+  estimated_minutes <- as.numeric(difftime(end_time, start_time, units = "mins")) * num_runs / num_test
+  cat("\n Estimated time for", num_runs,
+      "simulations:", round(estimated_minutes, 2), "minutes\n")
 }
-
 
 
 #' Performs a Monte Carlo simulation
@@ -56,39 +48,39 @@ MonteCarlo <- function(aTree,
                        nbRuns,
                        isFile = F,
                        verbose = T) {
-    if (verbose) {
-        cat("\n Time in: ", date())
-    }
+  if (verbose) {
+    cat("\n Time in: ", date())
+  }
 
-    MC <- matrix(ncol = nbRuns,
-                 nrow = aTree@NumberOfAttributes,
-                 dimnames = list(c(aTree@Attributes), c(seq(1:nbRuns))))
-    option <- matrix(nrow = aTree@NumberOfLeaves,
-                     ncol = nbRuns)
-    rownames(option) <- aTree@Leaves
+  MC <- matrix(ncol = nbRuns,
+               nrow = aTree@NumberOfAttributes,
+               dimnames = list(c(aTree@Attributes), c(seq(1:nbRuns))))
+  option <- matrix(nrow = aTree@NumberOfLeaves,
+                   ncol = nbRuns)
+  rownames(option) <- aTree@Leaves
 
-    for(k in aTree@Leaves) {
-        option[k, ] <- aTree@Nodes[[get_id(aTree@Nodes, k)[1]]]@RangeScale %>%
-            sample(size = nbRuns,
-                   prob = aTree@Nodes[[get_id(aTree@Nodes, k)[1]]]@Probability,
-                   replace = TRUE)
-    }
+  for(k in aTree@Leaves) {
+    option[k, ] <- aTree@Nodes[[get_id(aTree@Nodes, k)[1]]]@RangeScale %>%
+      sample(size = nbRuns,
+             prob = aTree@Nodes[[get_id(aTree@Nodes, k)[1]]]@Probability,
+             replace = TRUE)
+  }
 
-    MC <- 1:nbRuns %>%
-        sapply(function(x) {evaluate_scenario(aTree,
-                                             as.matrix(option[, x]))})
+  MC <- 1:nbRuns %>%
+    sapply(function(x) {evaluate_scenario(aTree,
+                                          as.matrix(option[, x]))})
 
-    if (isFile) {
-        # Write a file that contain all the random options selected for the MC analysis
-        write.table(option,
-                    file = "MC options.csv",
-                    sep = ",", row.names = T, col.names = NA)
-    }
+  if (isFile) {
+    # Write a file that contain all the random options selected for the MC analysis
+    write.table(option,
+                file = "MC options.csv",
+                sep = ",", row.names = T, col.names = NA)
+  }
 
-    if (verbose) {
-        cat("\n Time out: ",date())
-    }
-    return(MC)
+  if (verbose) {
+    cat("\n Time out: ",date())
+  }
+  return(MC)
 }
 
 
@@ -109,51 +101,51 @@ MonteCarlo <- function(aTree,
 #'
 #' @export
 ShowMC <- function(Node,
-                 MC,
-                 nbRuns) {
+                   MC,
+                   nbRuns) {
 
-    #  Node<-theTree@Nodes[[nodeName]]
-    typ <- "A"
-    if (Node@IsLeaf) { typ <- "L" }
+  #  Node<-theTree@Nodes[[nodeName]]
+  typ <- "A"
+  if (Node@IsLeaf) { typ <- "L" }
 
-    bar <- MC[Node@Name, ] %>%
-        tapply(MC[Node@Name, ], sum)
+  bar <- MC[Node@Name, ] %>%
+    tapply(MC[Node@Name, ], sum)
 
-    if (length(bar) < Node@RangeScale) {
-        newbar <- array(data = 0,
-                        dim = Node@RangeScale,
-                        dimnames = list(c(seq(1:Node@RangeScale))))
-        for(i in 1:dim(bar)) {
-            newbar[names(bar)[i]] <- bar[i]
-        }
-        bar <- newbar
+  if (length(bar) < Node@RangeScale) {
+    newbar <- array(data = 0,
+                    dim = Node@RangeScale,
+                    dimnames = list(c(seq(1:Node@RangeScale))))
+    for(i in 1:dim(bar)) {
+      newbar[names(bar)[i]] <- bar[i]
     }
+    bar <- newbar
+  }
 
-    bar[] <- (bar[] / c(1:Node@RangeScale)) / nbRuns
-    mc <- barplot(bar,
-                  main = paste(Node@Name, " [", typ, "]", sep = ""),
-                  xlab = "Modalities",
-                  ylab = "Frequencies",
-                  ylim = c(0, (max(bar) + 0.1)),
-                  las = 1, cex = 1, cex.main = 1, cex.lab = 1, cex.axis = 0.9)
-    #,names.arg=Node@ScaleLabel
+  bar[] <- (bar[] / c(1:Node@RangeScale)) / nbRuns
+  mc <- barplot(bar,
+                main = paste(Node@Name, " [", typ, "]", sep = ""),
+                xlab = "Modalities",
+                ylab = "Frequencies",
+                ylim = c(0, (max(bar) + 0.1)),
+                las = 1, cex = 1, cex.main = 1, cex.lab = 1, cex.axis = 0.9)
+  #,names.arg=Node@ScaleLabel
 
-    text(mc, bar,
-         format(round(bar, digits = 2)),
-         xpd = T, cex = 0.8, pos = 3)
+  text(mc, bar,
+       format(round(bar, digits = 2)),
+       xpd = T, cex = 0.8, pos = 3)
 
-    #  ref_points<-as.matrix(read.table(file="MC bar lengths_ref.csv",sep=",",row.names=1))
-    #  points(mc,as.vector(na.omit((ref_points))),pch=18,col=2)
+  #  ref_points<-as.matrix(read.table(file="MC bar lengths_ref.csv",sep=",",row.names=1))
+  #  points(mc,as.vector(na.omit((ref_points))),pch=18,col=2)
 
-    legend("topright",
-           legend = paste(names(bar), abbreviate(Node@ScaleLabel)),
-           box.lty = 0, cex =0.8)
-    text(length(bar))
+  legend("topright",
+         legend = paste(names(bar), abbreviate(Node@ScaleLabel)),
+         box.lty = 0, cex =0.8)
+  text(length(bar))
 
-    #write.table(bar,file="MC bar lengths.csv",sep=",",row.names=T,col.names=NA)
-    cat(row.names = Node@Name,
-        bar,
-        file = "MC bar lengths.csv",
-        sep = ",", fill = T, append = T)
-    return(bar)
+  #write.table(bar,file="MC bar lengths.csv",sep=",",row.names=T,col.names=NA)
+  cat(row.names = Node@Name,
+      bar,
+      file = "MC bar lengths.csv",
+      sep = ",", fill = T, append = T)
+  return(bar)
 }
