effb1c67f055a077cafcce42fe6922b487d603e8
rallart <roland.allart@inrae.fr>
2024-02-27 11:19:35 +0100

    fix: fix issue in test-aov
    they were order issues in the output visualisations. objects are the same but no shown in the same order. can't figure why. maybe due to an r update ?

--- tests/testthat/test-aov.R
+++ tests/testthat/test-aov.R
@@ -10,11 +10,18 @@ test_that("AOV : same output as previous version with small tree", {
 
   ### - Unit test - ###
   test_output <- aov_tree(tree)
+  sorted_test_output <- lapply(test_output, function(df){
+    df[order(rownames(df)),]
+  })
+
   expected_output <- readRDS(system.file("testdata", "test_aov_small.rds",
     package = "dexiranalysis"
   ))
+  sorted_expected_output <- lapply(expected_output, function(df){
+    df[order(rownames(df)),]
+  })
 
-  expect_equal(test_output, expected_output)
+  expect_equal(sorted_test_output, sorted_expected_output)
   ### - End - ###
 
   # restore random seed
